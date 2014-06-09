9e68a4604473d10895cb43c833563dc1fb77872d
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2014-06-09 17:39:38 +0200

    add basic configurations

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,33 +15,27 @@ require 'spree/core/testing_support/factories'
 require 'spree/core/testing_support/controller_requests'
 require 'spree/core/testing_support/authorization_helpers'
 require 'spree/core/url_helpers'
+require 'spree_gestpay/factories'
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
-
-  # == URL Helpers
-  #
-  # Allows access to Spree's routes in specs:
-  #
-  # visit spree.admin_path
-  # current_path.should eql(spree.products_path)
   config.include Spree::Core::UrlHelpers
 
-  # == Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
   config.mock_with :rspec
   config.color = true
+  config.use_transactional_fixtures = false
 
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.before :suite do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with :truncation
+  end
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
+  config.before do
+    DatabaseCleaner.strategy = example.metadata[:js] ? :truncation : :transaction
+    DatabaseCleaner.start
+  end
+
+  config.after do
+    DatabaseCleaner.clean
+  end
 end
--- spree_gestpay.gemspec
+++ spree_gestpay.gemspec
@@ -1,27 +1,25 @@
 # encoding: UTF-8
 Gem::Specification.new do |s|
-  s.platform  = Gem::Platform::RUBY
-  s.name      = 'spree_gestpay'
-  s.version   = '1.3.5'
-  s.summary   = 'GestPay Integration for Spree'
-  s.required_ruby_version = '>= 2.1.1'
+  s.platform     = Gem::Platform::RUBY
+  s.name         = 'spree_gestpay'
+  s.version      = '1.3.5'
+  s.summary      = 'GestPay Integration for Spree'
 
-  s.author    = 'Stefano Pigozzi'
-  s.email     = 'stefano.pigozzi@gmail.com'
-  # s.homepage  = 'http://www.spreecommerce.com'
-
-  #s.files       = `git ls-files`.split("\n")
-  #s.test_files  = `git ls-files -- {test,spec,features}/*`.split("\n")
+  s.author       = 'Stefano Pigozzi'
+  s.email        = 'stefano.pigozzi@gmail.com'
   s.require_path = 'lib'
   s.requirements << 'none'
 
+  s.required_ruby_version = '>= 2.1.1'
+
   s.add_dependency 'spree_core', '~> 1.3.5'
 
-  s.add_development_dependency 'capybara', '~> 2.0'
+  s.add_development_dependency 'capybara',     '~> 2.1'
   s.add_development_dependency 'coffee-rails'
   s.add_development_dependency 'factory_girl', '~> 4.2'
   s.add_development_dependency 'ffaker'
   s.add_development_dependency 'rspec-rails',  '~> 2.13'
   s.add_development_dependency 'sass-rails'
   s.add_development_dependency 'sqlite3'
+  s.add_development_dependency 'database_cleaner'
 end
