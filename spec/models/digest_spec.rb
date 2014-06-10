b304084a7e08286dcabaa3865c98c2eb8a807eb2
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2014-06-10 16:50:14 +0200

    digest update rspec api usage

--- spec/models/digest_spec.rb
+++ spec/models/digest_spec.rb
@@ -23,8 +23,8 @@ describe Gestpay::Digest do
         }
       end
 
-      it { should respond_to(:success?) }
-      it { should be_success }
+      it { is_expected.to respond_to(:success?) }
+      it { is_expected.to be_success }
       it { expect(subject.encrypted_string).to include('i3eUhxjb5HNxldtVSXb4qxaYva_uWKNn6BSQgRXkBUWKjoQvU6IbNgocbJYSr2LZ3FdnsvnpXH8KQ1') }
     end
 
@@ -32,7 +32,7 @@ describe Gestpay::Digest do
       let(:cassette) { 'encrypt_ko' }
       let(:hash) { Hash.new }
 
-      it { should_not be_success }
+      it { is_expected.not_to be_success }
     end
   end
 
@@ -41,7 +41,7 @@ describe Gestpay::Digest do
       let(:cassette) { 'decrypt_ok' }
       let(:result ) { digest.decrypt('rxd4jKk3CN8PbPX7gHh9u60ocD8_R85bI0NR_C*SOk1r1y2P8E9fDBgXKmmMnyyhyosyF8X6rvyTs_GCulPAy8TZKJc99tSb57cAWuo36MZ9_OWp2pwgKYLx7jS9XaLBgAqBufiFIoPq*Pwj5SdHY06vUf3ebnKryDvRnC0oldLw1YxpkIxlebYd7YxvNfSGLfSvce2NN8wWcwPfdR2jPKb*_oJpCEVZNO5TVMaH6h1UONEksNLIuDeZPqWfrHRcgbi0MLuoifb7ZBGrTw7HlEqfF6ojKt2oihKtNdn3Y3W5MkBHCsBrWq63TLYlDU8PRaV20jAdqQIJBHopVyumMGtyLoHlMFaH*nXUvgzwmYnTtwQZ*Q1nGCP0cl4YilBcivSTJCOkcrRwUhqYOl*4F1kmT15Nkp_sBB4oJRODYs8') }
 
-      it { should be_success }
+      it { is_expected.to be_success }
       it { expect(subject.buyer_name).to  eq "Test Customer" }
       it { expect(subject.buyer_email).to eq "test@example.com" }
       it { expect(subject.shop_transaction_id).to eq '123' }
@@ -51,7 +51,7 @@ describe Gestpay::Digest do
       let(:cassette) { 'decrypt_ko' }
       let(:result ) { digest.decrypt('KFzOej9MGr*lXEe*LOB2wzLmHja3ghKAo3iMpR1pG35moFdejWKzAj1Zd*R_e0x8qyOSTQmZVGRXEzJMVzd9xRXS*7tkbQLPOuBZ7qAtoFsX2Fxks5HxY*3YsUeiOInUXNF7*ih1bzkwmYGchDqYnEi31rN8ZYwLHnjwMIEmWJNn7oJHE6RpfUHG*7qohDxZRto*VVTPFdM93CwECq7SdYLebAQ_9EefdcE90QCRcFdGxZp_yAr_t*YRgLcZ5B5j843krtjCvf_m8nS99CroJQ768XsvlXy29McM2G*369mTufGyGMRab5PIvJlpSd3ds6mGi1IoMJAyXVkCu_ygRDDXN5Fk4Hz58LrDIci4ZszyVRcqy56SeYV7eG*kG8LZV40WV2ey8Qeh8XDUZnCV3A') }
 
-      it { should_not be_success }
+      it { is_expected.not_to be_success }
       it { expect(subject.error).to eq "Error 74: Autorizzazione negata" }
     end
   end
