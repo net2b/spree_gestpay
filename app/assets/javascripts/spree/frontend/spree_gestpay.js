b8c916747052d3026d658585c28715c1c395fd40
Alberto Vena <kennyadsl@gmail.com>
2015-06-22 16:50:14 +0200

    get iframe initialized (token request is ok)

---
+++ app/assets/javascripts/spree/frontend/spree_gestpay.js
@@ -0,0 +1,5 @@
+//= require spree/frontend/gestpay/module
+//= require spree/frontend/gestpay/logger
+//= require spree/frontend/gestpay/payment
+//= require spree/frontend/gestpay/iframe
+//= require spree/frontend/gestpay/init
--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -1,4 +1,4 @@
-module Spree::GestpayHelper
+Spree::BaseHelper.module_eval do
   def gestpay_iframe_script
     attrs =  {
       src:  Gestpay::Host.c2s/'pagam/JavaScript/js_GestPay.js',
