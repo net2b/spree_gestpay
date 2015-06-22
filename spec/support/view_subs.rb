f9605868459677569b6b92fb33dff7b4fd3b6024
Matteo Latini <mtylty@gmail.com>
2015-06-22 18:16:38 +0200

    makes some changes in assets placing

--- spec/support/view_subs.rb
+++ spec/support/view_subs.rb
@@ -1,7 +1,7 @@
 module ViewStubsHelpers
   def use_js_stubs(*list)
     list = list.map {|j| "store/gestpay/stubs/#{j}.js"}
-    instance = allow_any_instance_of(Spree::GestpayHelper)
+    instance = allow_any_instance_of(Spree::BaseHelper)
     instance.to receive(:gestpay_stub_scripts_list).and_return(list)
   end
 end
