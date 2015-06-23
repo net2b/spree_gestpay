e36c639293d608b50b4e6b0fc4eaf239c5f36a4c
Alberto Vena <kennyadsl@gmail.com>
2015-06-23 18:42:45 +0200

    make some other small steps ahead

--- app/controllers/spree/gestpay_iframe_callbacks_controller.rb
+++ app/controllers/spree/gestpay_iframe_callbacks_controller.rb
@@ -5,19 +5,8 @@ module Spree
     protect_from_forgery except: [:secure_3d]
 
     def secure_3d
-      # Not sure why opts is needed
-      opts = {}
-      opts[:buyer_email] = 'just@test.com'
-      # it = 1, en = 2, es = 3, fr = 4, de = 5
-      opts[:language_id] = (%w{ it en es fr de }.index(I18n.locale.to_s) || 1) + 1
-
-      @order = current_order
-      result = Spree::PaymentMethod.find(5).get_token(@order, opts)
-      if result.success?
-        session[:token] = result.encrypted_string
-      else
-        redirect_to cart_path, alert: result.error
-      end
+      @pares     = params["PaRes"].gsub(/\s+/, "")
+      @trans_key = params[:trans_key]
     end
   end
 end
