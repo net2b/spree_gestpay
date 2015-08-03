a279d1c127e2bec28a2d5830f6cf60b4b4687286
Alberto Vena <kennyadsl@gmail.com>
2015-08-03 18:55:56 +0200

    fix order completion with guest user

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Spree::Core::Engine.routes.draw do
   post '/checkout/payment/process_3d_redirect.json' => "gestpay#process_3d_redirect",
       as: :gestpay_3d_redirect
 
-  get '/checkout/payment/gestpay_completion' => "gestpay#completion",
+  get '/checkout/payment/gestpay_completion/:order_number' => "gestpay#completion",
       as: :gestpay_completion
 
   match '/gestpay_iframe_secure_3d' => 'gestpay_iframe_callbacks#secure_3d', via: [:post, :get], as: :secure_3d_callback
